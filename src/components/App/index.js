4fb0456c0cf79c5ffca4d65d80d79ac6ff3137ae
Lynne Rang <lynne.rang@gmail.com>
2019-07-15 17:46:10 -0600

    Add guts for Main component with grid for scores and board.

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './_App.scss';
 import Header from '../Header';
+import Main from '../Main';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Main />
     </div>
   );
 }
---
+++ src/components/Main/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const Main = () => {
+  return (
+    <main className="Main">
+      <div className="temp-score">
+        <h3>Team 1 Score</h3>
+        <p>3</p>
+        <p>Lynne</p>
+        <p>Justin</p>
+      </div>
+      <div className="temp-board">
+        Test
+      </div>
+      <div className="temp-score">
+        <h3>Team 2 Score</h3>
+        <p>5</p>
+        <p>Jon</p>
+        <p>Rachael</p>
+      </div>
+    </main>
+  );
+};
+
+export default Main;
