f538ba013ecc5b81d4d677afdb6e8011b57452c7
Lynne Rang <lynne.rang@gmail.com>
2019-07-15 17:05:34 -0600

    Add header component and aapp background image.

---
+++ src/components/Header/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header className="Header">
+      <div className="header-left">
+        <h1 className="header-title">CodeWords</h1>
+      </div>
+      <div className="header-right">
+        
+      </div>
+    </header>
+  );
+};
+
+export default Header;
