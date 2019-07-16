3d0475ec4a4306fc968d8313b295cd52f26f94b1
Lynne Rang <lynne.rang@gmail.com>
2019-07-16 15:09:20 -0600

    Add further styling to game board to match Figma design.

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -4,21 +4,16 @@ import Board from '../Board';
 import AgentInput from '../AgentInput';
 
 const Main = () => {
-  let status = "Active";
   return (
     <main className="Main">
       <Score team={1} score={5} players={["Lynne", "Justin"]}/>
-      <div className="temp-board">
-        <Board pType= {'intel'} />
-      </div>
+      {/* <div className="temp-board"> */}
+      <Board pType= {'intel'} />
+      {/* </div> */}
       <Score team={2} score={6} players={["Rachael", "Jon"]}/>
-      <div className="offset">
-      </div>
-      <div>
-        <AgentInput/>
-      </div>
-      <div className="offset">
-      </div>
+      <div className="offset"></div>
+      <AgentInput/>
+      <div className="offset"></div>
     </main>
   );
 };
