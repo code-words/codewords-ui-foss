143cbb48c4af9e9fefc9fa37d53df4a915528197
Rachael Drennan <rdrennan0@gmail.com>
2019-07-16 13:50:53 -0600

    Implement websockets for connection with Ruby on Rails

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from "./components/App";
 import * as serviceWorker from "./serviceWorker";
 import { BrowserRouter } from "react-router-dom";
 import { ActionCableProvider } from 'react-actioncable-provider';
-import { API_WS_ROOT } from './constants';
+import { API_WS_ROOT } from './variables';
 
 ReactDOM.render(
   <ActionCableProvider url={API_WS_ROOT}>
