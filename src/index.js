0383afc65c7bfc2db2b9ac4e2dc27253e8839c86
Rachael Drennan <rdrennan0@gmail.com>
2019-07-16 09:01:30 -0600

    add imports and variables folder for ActionCable

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.scss';
-import App from './components/App';
-import * as serviceWorker from './serviceWorker';
-import { BrowserRouter } from 'react-router-dom'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.scss";
+import App from "./components/App";
+import * as serviceWorker from "./serviceWorker";
+import { BrowserRouter } from "react-router-dom";
+import { ActionCableProvider } from 'react-actioncable-provider';
+import { API_WS_ROOT } from './constants';
 
-ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById('root'));
+ReactDOM.render(
+  <ActionCableProvider url={API_WS_ROOT}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </ActionCableProvider>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
