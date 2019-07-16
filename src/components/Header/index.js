bb1a9e61fdef58a9cbea40db45de962d89b4e385
SiimonStark <justinpyktel@gmail.com>
2019-07-16 00:08:03 -0600

    minor changes, change position of render

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,11 +1,20 @@
 import React from 'react';
 
 const Header = () => {
+  let status = "Active";
+// todo: FIX style
+  // let style = {
+  //   background: `$player-${status.toLowerCase()}`
+  // }
+
   return (
     <header className="Header">
       <div className="header-left">
         <h1 className="header-title">CodeWords</h1>
       </div>
+      <h2 className="status">Agent Status:
+        <span className={status}> {status}</span>
+      </h2>
       <nav className="header-right">
         <p>Instructions</p>
         <button className="nav-item new-game-btn">New Game</button>
