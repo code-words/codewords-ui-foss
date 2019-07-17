f6569df19e4c7c21052c699e128a807d7ec27fb0
SiimonStark <justinpyktel@gmail.com>
2019-07-16 20:01:50 -0600

    quick conflict fix, from remote branch

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -7,6 +7,7 @@ import NewGame from '../NewGame';
 import JoinGame from '../JoinGame';
 import Lobby from '../Lobby';
 import Main from '../Main';
+import ConversationsList from '../ConversationsList';
 
 function App() {
   return (
