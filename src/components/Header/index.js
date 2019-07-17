9c79f2abdba0a127c5c6ef0076a9cadba711261d
SiimonStark <justinpyktel@gmail.com>
2019-07-17 00:27:45 -0600

    add NavLink for RuleList

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -9,8 +9,10 @@ const Header = () => {
         <h1 className="header-title">CodeWords</h1>
       </div>
       <nav className="header-right">
-        <p>Instructions</p>
-        <NavLink to="/startScreen" className="nav-item new-game-btn">
+        <NavLink to="/ruleList" className="nav-item rules-nav">
+          Instructions
+        </NavLink>
+        <NavLink to="/startScreen" className="nav-item new-game-nav">
           New Game
         </NavLink>
       </nav>
