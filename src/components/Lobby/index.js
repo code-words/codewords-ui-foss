7b1e1890a0f0d699621536472401ff40dab37d87
SiimonStark <justinpyktel@gmail.com>
2019-07-17 12:02:45 -0600

    Start building Lobby file

--- src/components/Lobby/index.js
+++ src/components/Lobby/index.js
@@ -1,10 +1,38 @@
 import React from 'react';
 
 const Lobby = () => {
+  let players = [
+    {name: 'justin', team: 'blue'},
+    {name: 'LyNne', team: 'red'}
+  ];
+
+  const renderJoinedPlayers = (side) => {
+    let teamPlayers = players.filter(p => {
+      return p.team === side;
+    }) 
+
+    console.log('Teams: ', teamPlayers)
+    return teamPlayers.map(p => {
+      return (<p>{p.name.toUpperCase()}</p>)
+    })
+  };
+  
   return (
     <div className="backdrop">
       <section className="Lobby">
         <h3>Please wait for remaining players...</h3>
+        <section className="team-rosters">
+          <div className="team-blue">
+            <h4>Blue Team</h4>
+            <hr />
+            {renderJoinedPlayers('blue')}
+          </div>
+          <div className="team-red">
+            <h4>Red Team</h4>
+            <hr />
+            {renderJoinedPlayers('red')}
+          </div>
+        </section>
       </section>
     </div>
   )
