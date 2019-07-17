20effb1544c54e1a6da2334aef778a2e1565b84c
Rachael Drennan <rdrennan0@gmail.com>
2019-07-17 17:19:58 -0600

    Add cableprovider npm

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -8,19 +8,47 @@ import JoinGame from '../JoinGame';
 import Lobby from '../Lobby';
 import Main from '../Main';
 import ErrorScreen from '../ErrorScreen';
+import Cable from 'actioncable';
+
 
 export class App extends Component {
   constructor(){
     super()
     this.state={
-      socket: "0000"
+      token: "000"
     }
   }
 
-  handleWebSocket = ({name,value}) => {
+  handleUserInit = ({name,token}) => {
+    console.log(token)
    this.setState({
-     socket:value
-   })
+     token
+   }, () => this.createCable(token) )
+   
+  }
+  createCable = (token) => {
+    if(this.state.token === token ){
+      let cable = Cable.createConsumer(`ws://localhost:3000/cable/${token}`);
+      console.log(cable)
+      this.chats = cable.subscriptions.create({
+        channel: 'GameDataChannel'
+      }, {
+        connected: () => {console.log("connected")},
+        disconnected: () => { console.log("disconnected")},
+        rejected: () => { console.log("rejected")},
+        received: (data) => {
+          console.log(data)
+          // let chatLogs = this.state.chatLogs;
+          // chatLogs.push(data);
+          // this.setState({ chatLogs });
+        },
+        create: function(chatContent) {
+          this.perform('create', {
+            content: "hello"
+          });
+        }
+      });
+    }
   }
  
    render(){
@@ -30,12 +58,12 @@ export class App extends Component {
            <Switch>
              <Route exact path='/' component={StartScreen} />
              <Route exact path='/rules' component={RuleList}/>
-             <Route exact path='/new' component={NewGame} />
+             <Route exact path='/new' render={()=> <NewGame handleUserInit={this.handleUserInit}/>} />
              <Route exact path='/join' component={JoinGame} />
              <Route exact path='/lobby' component={Lobby} />
              <Route exact path='/game' component={Main} />
              <Route component={ErrorScreen} />
-             <Route path='/' render={() => <Main websocket={this.state.socket}/>}/>
+             <Route path='/' render={() => <Main token={this.state.token}/>}/>
              {/* <ConversationsList /> */}
          </Switch>
        </div>
