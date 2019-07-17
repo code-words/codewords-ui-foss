b86c397428b391b4d3fd2bafde4aee3162a3fcb2
Lynne Rang <lynne.rang@gmail.com>
2019-07-17 10:02:03 -0600

    Add error route.

---
+++ src/components/ErrorScreen/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './_ErrorScreen.scss';
+
+const ErrorScreen = () => {
+  return (
+    <div id="ErrorScreen">
+      <h2>404 Page Not Found :(</h2>
+    </div>
+  );
+};
+
+export default ErrorScreen;
