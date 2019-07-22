71841f6969254a634823b6f37ceffe97d33baa2b
Rachael Drennan <rdrennan0@gmail.com>
2019-07-22 12:08:20 -0600

    make Main stateful

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,23 +1,26 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { ActionCableProvider } from 'react-actioncable-provider';
 import { API_WS_ROOT } from '../../variables';
 import Score from './Score';
 import Board from './Board';
 import AgentInput from './AgentInput';
 
-const Main = (props) => {
-  return (
-    <main className="Main">
-    <ActionCableProvider url={API_WS_ROOT} socket={props.socket}>
-      <Score team={1} score={5} players={["Lynne", "Justin"]}/>
-      <Board pType= {'intel'} />
-      <Score team={2} score={6} players={["Rachael", "Jon"]}/>
-      <div className="offset"></div>
-      <AgentInput websocket={props.websocket} hintLogs ={props.hintLogs}/>
-      <div className="offset"></div>
-      </ActionCableProvider>
-    </main>
-  );
-};
+class Main extends Component {
+  render() {
+    console.log(this.props)
+    return (
+      <main className="Main">
+      <ActionCableProvider url={API_WS_ROOT} socket={this.props.socket}>
+        <Score team={1} score={5} players={["Lynne", "Justin"]}/>
+        <Board pType= {'intel'} />
+        <Score team={2} score={6} players={["Rachael", "Jon"]}/>
+        <div className="offset"></div>
+        <AgentInput cable = {this.props.cable} websocket={this.props.websocket} hintLogs ={this.props.hintLogs}/>
+        <div className="offset"></div>
+        </ActionCableProvider>
+      </main>
+    );
+  }
+}
 
-export default Main;
+export default Main
