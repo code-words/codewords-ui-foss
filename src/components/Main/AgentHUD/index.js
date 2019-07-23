a73de778cf6cda556e0627181ee68ca1a9b79089
SiimonStark <justinpyktel@gmail.com>
2019-07-23 17:35:54 -0600

    Create component for displaying the hint

---
+++ src/components/Main/AgentHUD/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AgentHUD = (props) => {
+  return (
+    <article className="AgentInput">
+      <p>Waiting for hint...</p>
+    </article>
+  )
+}
+
+export default AgentHUD;
