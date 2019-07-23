cd8a4ee57508ebbcddd0130e912b4cf861203e21
SiimonStark <justinpyktel@gmail.com>
2019-07-22 21:37:39 -0600

    Write input, submit, and render tests

--- src/components/JoinGame/index.test.js
+++ src/components/JoinGame/index.test.js
@@ -4,16 +4,122 @@ import { shallow } from 'enzyme';
 
 describe('JoinGame', () => {
   let wrapper;
-  let instance;
+  let mockState;
+  let handleUserInitMock = jest.fn();
 
   beforeEach(() => {
-    wrapper = shallow(<JoinGame />);
-    instance = wrapper.instance();
-
-  })
+    wrapper = shallow(<JoinGame handleUserInit={handleUserInitMock} />);
+    mockState = {
+      inviteCode: '',
+      name: '',
+      redirect: false
+    }
+  });
 
   it('should match component snapshot', () => {
     expect(wrapper).toMatchSnapshot();
   });
 
+  it('should update state:[inviteCode] on user input', () => {
+    expect(wrapper.state()).toEqual(mockState);
+
+    const input = 'XE3T';
+    const expectedState = { ...mockState };
+    expectedState.inviteCode = input;
+    const mockEvent = {
+      target: {
+        name: 'inviteCode',
+        value: input
+      }
+    }
+    wrapper.instance().handleChange(mockEvent);
+
+    expect(wrapper.state()).toEqual(expectedState);
+  });
+
+  it('should update state:[name] on user input', () => {
+    expect(wrapper.state()).toEqual(mockState);
+
+    const input = 'testUser';
+    const expectedState = { ...mockState };
+    expectedState.name = input;
+    const mockEvent = {
+      target: {
+        name: 'name',
+        value: input
+      }
+    }
+    wrapper.instance().handleChange(mockEvent);
+
+    expect(wrapper.state()).toEqual(expectedState);
+  });
+
+  it('should update state to redirect on submit', () => {
+    // Expect Default
+    expect(wrapper.state()).toEqual(mockState);
+
+    // Setup for input:[inviteCode]
+    let expectedState = { ...mockState };
+    expectedState.inviteCode = 'XE3T';
+    let mockEvent = {
+      target: {
+        name: 'inviteCode',
+        value: 'XE3T'
+      }
+    }
+    // Execution for input:[inviteCode]
+    wrapper.instance().handleChange(mockEvent);
+    // Evaluate for input:[inviteCode]
+    expect(wrapper.state()).toEqual(expectedState);
+
+    // Setup for input:[name]
+    expectedState.name = 'testUser';
+    mockEvent = {
+      target: {
+        name: 'name',
+        value: 'testUser'
+      }
+    }
+    // Execution for input:[name]
+    wrapper.instance().handleChange(mockEvent);
+    // Evaluate for input:[name]
+    expect(wrapper.state()).toEqual(expectedState);
+
+    // Setup for submit
+    expectedState.redirect = true;
+    mockEvent = {
+      preventDefault: () => { }
+    }
+    // Execution for submit
+    wrapper.instance().handleSubmit(mockEvent)
+    // Evaluate for submit
+    expect(wrapper.state()).toEqual(expectedState);
+  });
+//  =======================================
+// Todo: =================================
+//  =======================================
+  it.skip('should call handleUserInit w/ response from Post', async () => {
+    let mockEvent = {
+      target: {
+        name: 'name',
+        value: 'testUser'
+      }
+    }
+    wrapper.instance().handleChange(mockEvent);
+
+    mockEvent = {
+      target: {
+        name: 'inviteCode',
+        value: 'XE3T'
+      }
+    }
+    wrapper.instance().handleChange(mockEvent);
+
+    mockEvent = {
+      preventDefault: () => { }
+    }
+    wrapper.instance().handleSubmit(mockEvent)
+
+    expect(handleUserInitMock).toHaveBeenCalled()
+  });
 })
--- src/components/Lobby/index.test.js
+++ src/components/Lobby/index.test.js
@@ -1,19 +1,41 @@
 import React from 'react';
 import Lobby from '.';
-import { shallow } from 'enzyme';
+import { MemoryRouter } from 'react-router';
+import App from '../App';
+import { mount } from 'enzyme';
 
 describe('Lobby', () => {
   let wrapper;
   let instance;
+  let mockPlayers;
 
   beforeEach(() => {
-    wrapper = shallow(<Lobby />);
+    mockPlayers = [
+      { name: 'test1', id: 1 },
+      { name: 'test2', id: 2 }
+    ];
+    wrapper = mount(
+      <MemoryRouter initialEntries={['/lobby']}>
+        <App>
+          <Lobby players={mockPlayers} />
+        </App>
+      </MemoryRouter>
+    );
     instance = wrapper.instance();
-
   })
 
   it('should match component snapshot', () => {
     expect(wrapper).toMatchSnapshot();
   });
+
+  it('should match snapshot after a player joins', () => {
+    expect(wrapper).toMatchSnapshot();
+    expect(mockPlayers.length).toEqual(2);
+
+    mockPlayers.push({name: 'test3', id: 3 });
+
+    expect(wrapper).toMatchSnapshot();
+    expect(mockPlayers.length).toEqual(3);
+  })
 
 })
