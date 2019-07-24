fc536f476681ffdd750f24f6b7156a028c4df8cc
Jon Peterson <3322920+joequincy@users.noreply.github.com>
2019-07-24 16:49:35 -0600

    Use HTTPS in production

--- src/variables/index.js
+++ src/variables/index.js
@@ -1,19 +1,22 @@
 let GAME_HOST;
+let SECURE = 's';
 switch (window.location.hostname) {
   case 'localhost':
     GAME_HOST = 'localhost:3000';
+    SECURE = '';
     break;
   case 'playcodewords.com':
   case 'www.playcodewords.com':
-    GAME_HOST = 'codewords-server.herokuapp.com'
+    GAME_HOST = 'codewords-server.herokuapp.com';
+    break;
   default:
     GAME_HOST = window.location.hostname.replace('game', 'server');
 }
 
 console.dir(process.env);
 
-export const API_ROOT = `http://${GAME_HOST}/api`;
-export const API_WS_ROOT = `ws://${GAME_HOST}/cable`;
+export const API_ROOT = `http${SECURE}://${GAME_HOST}/api`;
+export const API_WS_ROOT = `ws${SECURE}://${GAME_HOST}/cable`;
 export const HEADERS = {
   'Content-Type': 'application/json',
   Accept: 'application/json',
