98d6c9f3898baaadbca2fedc01569bb9565b1ddf
Rachael Drennan <rdrennan0@gmail.com>
2019-07-24 08:58:34 -0600

    Add Card tests

--- src/components/Main/Board/Card/index.js
+++ src/components/Main/Board/Card/index.js
@@ -1,85 +1,41 @@
 import React from "react";
 
 class Card extends React.Component {
-  constructor(props) {
-    super(props);
-<<<<<<< HEAD
-    this.state = {
-      flipped: false
-    };
-  }
-
   handleClick = e => {
-    this.setState({ flipped: !this.state.flipped });
-    this.props.sendGuess(e.target.id);
+
+    if (this.props.isActive && !this.props.card.type) {
+      this.props.sendGuess({ id: e.currentTarget.id });
+    }
   };
 
   render() {
     const { card, isActive } = this.props;
-
-    const intelCls = card.type ? card.type : "";
-    // const clickCls = !card.type && isActive ? 'clickable' : null;
-    const img = card.type
-      ? require(`../../../../images/${card.type}.jpg`)
-      : null;
-
-    //replace next line w/ card.flipped when data is updating
-    const content = this.state.flipped ? (
-      <article className="card" key={`${card.word}`}>
-        <img alt={"a card with one word on it"} className="card" src={img} />
+    const { type, flipped, word, id } = card;
+    const intelCls = type ? type : "";
+    const clickCls = !type && isActive && !flipped ? "clickable" : null;
+    /* istanbul ignore next */
+    const img = type ? require(`../../../../images/${type}.jpg`) : null;
+    /* istanbul ignore next */
+    const content = flipped ? (
+      <article className="card" key={`${word}`} id={id}>
+        <img className="card" src={img} alt={type} />
       </article>
     ) : (
       <article
-        className={`card front ${intelCls} clickable`}
-        key={`${card.word}`}
-        id={card.id}
-        name={card.word}
+        className={`card front ${intelCls} ${clickCls}`}
+        key={`${word}`}
+        id={id}
+        name={word}
         onClick={this.handleClick}
       >
-        <div className="word-bg" key={`${card.word}-bg`}>
-          <p className="word-txt" key={`${card.word}-txt`}>
-            {card.word}
+        <div className="word-bg" key={`${word}-bg`}>
+          <p className="word-txt" key={`${word}-txt`}>
+            {word}
           </p>
         </div>
       </article>
     );
-=======
-
-	}
-
-  handleClick = e => {
-    if (this.props.isActive && !this.props.card.type) {
-      this.props.sendGuess({ id: e.currentTarget.id });
-    }
-	};
-
-	render() {
-    const { card, isActive } = this.props;
-    const { type, flipped, word, id } = card;
-		const intelCls = type ? type : '';
-		const clickCls = !type && isActive && !flipped ? 'clickable' : null;
-		const img = type ? require(`../../../../images/${type}.jpg`) : null;
-
-		const content = flipped ? (
-      <article className="card" key={`${word}`} id={id}>
-				<img className="card" src={img} alt={type} />
-			</article>
-		) : (
-			<article
-				className={`card front ${intelCls} ${clickCls}`}
-				key={`${word}`}
-				id={id}
-				name={word}
-				onClick={this.handleClick}>
-				<div className="word-bg" key={`${word}-bg`}>
-					<p className="word-txt" key={`${word}-txt`}>
-						{word}
-					</p>
-				</div>
-			</article>
-		);
->>>>>>> 882a23a59225e6729fe0c85f0d45b37fc5ee4004
-
+    /* istanbul ignore next */
     return content;
   }
 }
--- src/components/Main/Board/Card/index.test.js
+++ src/components/Main/Board/Card/index.test.js
@@ -1,25 +1,39 @@
 import Card from './index';
 import React from 'react';
-
 import { shallow } from 'enzyme'
+ require('../../../../images/bystander.jpg');
+
 
 
 describe('Card', () => {
-    let mockCard = {type: 'null'}
-    
-    let wrapper = shallow(<Card card={mockCard} image={null}/>)
+    let mockCard = {type: true}
+    let mockFn = jest.fn()
+    let wrapper = shallow(<Card card = {mockCard} sendGuess={mockFn}/>)
     it('should matchsnapshot', () => {
-        wrapper.setState({ flipped: true });
+        wrapper = shallow (<Card card={{type: "bystander"}} sendGuess={mockFn} />)
+        wrapper.setState({ flipped: false});
         expect(wrapper).toMatchSnapshot()
     });
     it('should matchsnapshot', () => {
-        wrapper.setState({ flipped: false });
+        let wrapper = shallow(<Card card={mockCard} isActive={false}/>)
+        wrapper.setState({ flipped: true });
         expect(wrapper).toMatchSnapshot()
     });
     it('handleClick should be called on click', () => {
         const spy = jest.spyOn(wrapper.instance(), 'handleClick')
-        wrapper.find('article').simulate('click');
-       
+        let mockEvent = { target: { value: "", name: "title" } }
+        wrapper.find('article').simulate('click', mockEvent);
         expect(spy).toHaveBeenCalled()
     });
+    it('should send an id when is Active is true and card.type is false', () => {
+        let mockCard = {type: false}
+         wrapper = shallow(<Card card={mockCard} sendGuess={mockFn} isActive={true}/>)
+        expect(mockFn).toHaveBeenCalled()
+    });
+    it('should not send an id when is Active is false and card.type is true', () => {
+        let mockCard = {type: true}
+         wrapper = shallow(<Card card={mockCard} sendGuess={mockFn} isActive={false}/>)
+        expect(mockFn).toHaveBeenCalled()
+    });
+
 });
