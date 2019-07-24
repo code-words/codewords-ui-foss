885b0012df3a58db361c34ed99fb8c6ae5a11c80
Rachael Drennan <rdrennan0@gmail.com>
2019-07-24 08:59:41 -0600

    AgentInput testing

--- src/components/Main/AgentInput/index.js
+++ src/components/Main/AgentInput/index.js
@@ -1,97 +1,93 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class AgentInput extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			hintWord: '',
-			numCards: 1,
-			hintLogs: []
-		};
+  constructor(props) {
+    super(props);
+    this.state = {
+      hintWord: "",
+      numCards: 1,
+      hintLogs: []
+    };
   }
-  
+
   componentDidMount = () => {
     let hintLogs = this.props.hintLogs;
 
     this.setState({ hintLogs });
   };
 
-	handleChange = e => {
+  handleChange = e => {
     const { name, value } = e.target;
-    
-		this.setState({ [name]: value });
-	};
 
-	handleSendEvent(e) {
-		e.preventDefault();
-		const { hintWord, numCards } = this.state;
-		let hint = { hintWord, numCards };
+    this.setState({ [name]: value });
+  };
 
-		this.props.cable.sendHint(hint);
-		this.setState({ hintWord: '', numCards: 1 });
-	}
+  handleSendEvent(e) {
+    e.preventDefault();
+    const { hintWord, numCards } = this.state;
+    let hint = { hintWord, numCards };
 
-	renderChatLog() {
-		// return this.state.hintLogs.map((chat) => {
-		//   return (
-		//     <li key={`chat_${chat.id}`}>
-		//       <span className='chat-message'>{ chat.content }</span>
-		//       <span className='chat-created-at'>{ chat.created_at }</span>
-		//     </li>
-		//   );
-		// })
-	}
-
-	handleChatInputKeyPress(event) {
-		if (event.key === 'Enter') this.handleSendEvent(event);
+    this.props.cable.sendHint(hint);
+    this.setState({ hintWord: "", numCards: 1 });
   }
 
-	// updateCurrentChatMessage({target}) {
-	//     this.setState({
-	//       hint: target.value
-	//     });
-	// }
+  renderChatLog() {
+    // return this.state.hintLogs.map((chat) => {
+    //   return (
+    //     <li key={`chat_${chat.id}`}>
+    //       <span className='chat-message'>{ chat.content }</span>
+    //       <span className='chat-created-at'>{ chat.created_at }</span>
+    //     </li>
+    //   );
+    // })
+  }
+  handleChatInputKeyPress(event) {
+    if (event.key === "Enter") this.handleSendEvent(event);
+  }
+  // updateCurrentChatMessage({target}) {
+  //     this.setState({
+  //       hint: target.value
+  //     });
+  // }
 
-	render() {
-		console.log('Agent State', this.state);
-		return (
-			<form className="AgentInput" onSubmit={e => this.handleSendEvent(e)}>
-				{/* <ul className='chat-logs'>
+  render() {
+    return (
+      <form className="AgentInput" onSubmit={e => this.handleSendEvent(e)}>
+        {/* <ul className='chat-logs'>
           { this.renderChatLog() }
         </ul> */}
-				<div className="hint-entries">
-					<label htmlFor="hintWord">Hint:</label>
-					<input
-						name="hintWord"
-						onKeyPress={e => this.handleChatInputKeyPress(e)}
-						value={this.state.hint}
-						onChange={e => this.handleChange(e)}
-						type="text"
-						placeholder="Enter your hint..."
-						className="chat-input"
-						disabled={!this.props.isActive}
-					/>
-					<label htmlFor="num-input">Quantity:</label>
-					<input
-						className="num-input"
-						name="numCards"
-						type="number"
-						value={this.state.numCards}
-						onChange={this.handleChange}
-						disabled={!this.props.isActive}
-					/>
+        <div className="hint-entries">
+          <label htmlFor="hintWord">Hint:</label>
+          <input
+            name="hintWord"
+            onKeyPress={e => this.handleChatInputKeyPress(e)}
+            value={this.state.hint}
+            onChange={e => this.handleChange(e)}
+            type="text"
+            placeholder="Enter your hint..."
+            className="chat-input"
+            disabled={!this.props.isActive}
+          />
+          <label htmlFor="num-input">Quantity:</label>
+          <input
+            className="num-input"
+            name="numCards"
+            type="number"
+            value={this.state.numCards}
+            onChange={this.handleChange}
+            disabled={!this.props.isActive}
+          />
           <button
             onClick={e => this.handleSendEvent(e)}
             className="btn-default send-btn"
             disabled={!this.props.isActive}
           >
-						Send
-					</button>
-				</div>
-			</form>
-		);
-	}
-
+            Send
+          </button>
+        </div>
+      </form>
+    );
+  }
 }
 
 export default AgentInput;
--- src/components/Main/AgentInput/index.test.js
+++ src/components/Main/AgentInput/index.test.js
@@ -4,16 +4,40 @@ import { shallow } from 'enzyme';
 
 describe('AgentInput', () => {
   let wrapper;
-  let instance;
+  let mockFn= {sendHint: jest.fn()}
 
   beforeEach(() => {
-    wrapper = shallow(<AgentInput />);
-    instance = wrapper.instance();
+    wrapper = shallow(<AgentInput  cable={mockFn}/>);
 
   })
 
   it('should match component snapshot', () => {
     expect(wrapper).toMatchSnapshot();
-  });
+  })
+  it('should call handleChange on input', () => {
+     let spy = jest.spyOn(wrapper.instance(), "handleChange");
+     wrapper.find('.chat-input').simulate("change", {target:{name: "test", value: "testing"}});
+    expect(spy).toHaveBeenCalled()
+  })
+  it('should call handleChatInputKeyPress on keypress', () => {
+    let spy = jest.spyOn(wrapper.instance(), "handleChatInputKeyPress");
+    wrapper.find('.chat-input').simulate("keyPress", {key: "Enter", preventDefault: jest.fn()});
+   expect(spy).toHaveBeenCalled()
+ })
+ it('should update state when handleSendEvent is called', () => {
+  wrapper.setState({ hintWord: 'testing' });
+  wrapper.instance().handleSendEvent({preventDefault: jest.fn()})
+  expect(wrapper.state('hintWord')).toEqual("");
+ });
+ it('should call handleSendEvent when form is submitted', () => {
+  let spy = jest.spyOn(wrapper.instance(), "handleSendEvent");
+  wrapper.find('form').simulate("submit", {key: "Enter", preventDefault: jest.fn()});
+   expect(spy).toHaveBeenCalled()
+ });
+ it('should call handleSendEvent when button is clicked', () => {
+  let spy = jest.spyOn(wrapper.instance(), "handleSendEvent");
+  wrapper.find('.send-btn').simulate("click", {target:{name: "test", value: "testing"}, preventDefault: jest.fn()});
+  expect(spy).toHaveBeenCalled()
+ });
   
 })
