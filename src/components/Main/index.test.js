18aad989933e49cb46e1e89063ce4161d5923939
Lynne Rang <lynne.rang@gmail.com>
2019-07-25 08:38:47 -0600

    Add fixes for Main component tests by adding more props.

--- src/components/Main/index.test.js
+++ src/components/Main/index.test.js
@@ -4,11 +4,17 @@ import { shallow } from 'enzyme';
 
 describe('Main', () => {
 	let wrapper;
-	let mockPlayers = [ { players: { blueIntel: { name: 'test' } } }, { players: { blueIntel: { name: 'test' } } } ];
+  // let mockPlayers = [{ players: { blueIntel: { name: 'test' } } }, { players: { blueIntel: { name: 'test' } } }];
+  let mockPlayers = [
+    { isIntel: true, isBlueTeam: true, name: "Bob1" },
+    { isIntel: true, isBlueTeam: false, name: "Bob2" },
+    { isIntel: false, isBlueTeam: true, name: "Bob3" },
+    { isIntel: false, isBlueTeam: false, name: "Bob4" }
+  ];
 	let mockScores = { blueScore: 0, redScore: 0 };
 
 	beforeEach(() => {
-		wrapper = shallow(<Main cardData={[ { id: 3, type: 'red' } ]} scores={mockScores} players={mockPlayers} />);
+    wrapper = shallow(<Main cardData={[{ id: 3, type: 'red' }]} scores={mockScores} user={{name: 'Bob'}} players={mockPlayers} />);
 	});
 
 	it('should match component snapshot', () => {
