8ba1794d4b25aab03d8555714342234a59939f7c
Rachael Drennan <rdrennan0@gmail.com>
2019-07-25 08:26:47 -0600

    Update main and board tests

--- src/components/Main/index.test.js
+++ src/components/Main/index.test.js
@@ -3,16 +3,13 @@ import Main from '.';
 import { shallow } from 'enzyme';
 
 describe('Main', () => {
-	let wrapper;
 	let mockPlayers = [ { players: { blueIntel: { name: 'test' } } }, { players: { blueIntel: { name: 'test' } } } ];
 	let mockScores = { blueScore: 0, redScore: 0 };
+	let wrapper = shallow(<Main cardData={[ { id: 3, type: 'red' } ]} scores={mockScores} players={mockPlayers} />);
 
-	beforeEach(() => {
-		wrapper = shallow(<Main cardData={[ { id: 3, type: 'red' } ]} scores={mockScores} players={mockPlayers} />);
-	});
 
 	it('should match component snapshot', () => {
-		expect(wrapper).toMatchSnapshot();
+		expect(wrapper.debug()).toMatchSnapshot();
 	});
 });
 
